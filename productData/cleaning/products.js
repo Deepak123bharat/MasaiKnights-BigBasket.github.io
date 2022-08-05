a65d45a2299749fa2cab8a0966aae7d5c01fc274
rajtilak-patel <rp2380264@gmail.com>
2022-08-05 16:02:32 +0530

    added cleaning eggs kitchens

---
+++ productData/cleaning/products.js
@@ -0,0 +1,230 @@
+
+// cleaning
+var dairyArr = [
+    {
+        image_adress:"https://www.bigbasket.com/pd/1207190/harpic-original-disinfectant-toilet-cleaner-liquid-3x1-l/?nc=cl-prod-list&t_pg=l1-cat-cleaning-household&t_p=cl-temp-1&t_s=cl-prod-list&t_pos=3&t_ch=desktop",
+        brand:"Harpic",
+        name : "Disinfectant Toilet Cleaner Liquid - Originalc",
+        Quantity : "1 L",
+        price : "Rs 567.40",
+        strikedOdprice : "Rs 585.60"
+
+    },
+    {
+        
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/40081006_10-harpic-disinfectant-bathroom-cleaner-liquid-lemon.jpg",
+        brand:"Harpic",
+        name : "Disinfectant Bathroom Cleaner Liquid, Lemon",
+        Quantity : "1 L",
+        price : "Rs 172.90",
+        strikedOdprice : "Rs 180.00"
+
+    },
+    
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/266945_21-surf-excel-easy-wash-detergent-powder.jpg",
+        brand:"Harpic",
+        name : "Easy Wash Detergent Powder",
+        Quantity : "1.5 Kg",
+        price : "Rs 199",
+        strikedOdprice : "Rs 226"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/40200751_1-vim-dishwash-liquid-gel-lemon-refill-pouch.jpg",
+        brand:"Harpic",
+        name : "Dishwash Liquid Gel Lemon Refill Pouch",
+        Quantity : "2 L",
+        price : "Rs 355",
+        strikedOdprice : "Rs 445"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/40129070_8-lizol-disinfectant-surface-floor-cleaner-liquid-citrus-kills-999-germs.jpg",
+        brand:"Vim",
+        name : "Disinfectant Surface & Floor Cleaner Liquid - Citrus",
+        Quantity : "5 L",
+        price : "Rs 781.08",
+        strikedOdprice : "Rs 840"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/40038652_2-vim-dishwash-bar-lemon.jpg",
+        brand:"Vim",
+        name : "Dishwash Bar - Lemon",
+        Quantity : "60 G",
+        price : "Rs 5",
+        strikedOdprice : "Rs 5.50"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/1208975_4-harpic-harpic-bathroom-cleaner-lemon-1-l-harpic-toilet-cleaner-original-1-l.jpg",
+        brand:"Vim",
+        name : "Bathroom Cleaner Lemon 1 L + Toilet Cleaner, Original 1 L",
+        Quantity : "1 L + 1 L",
+        price : "Rs 362.05",
+        strikedOdprice : "Rs 394.05"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/230745_18-comfort-after-wash-morning-fresh-fabric-conditioner.jpg",
+        brand:"Vim",
+        name : "After Wash Morning Fresh Fabric Conditioner",
+        Quantity : "860ml",
+        price : "Rs 205",
+        strikedOdprice : "Rs 225"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/40130608_7-comfort-after-wash-morning-fresh-fabric-conditioner.jpg",
+        brand:"Vim",
+        name : "After Wash Fabric Conditioner - Morning Fresh",
+        Quantity : "2 L",
+        price : "Rs 430",
+        strikedOdprice : "Rs 480"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/40195406_4-ariel-matic-front-load-liquid-detergent.jpg",
+        brand:"Surf Excel",
+        name : "Matic Front Load Liquid Detergent",
+        Quantity : "1 L",
+        price : "Rs 200.00",
+        strikedOdprice : "Rs 250.00"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/40222463_1-surf-excel-matic-liquid-detergent-front-load-refill-pack.jpg",
+         brand:"Surf Excel",
+        name : "Matic Liquid Detergent - Front Load, Refill Pack",
+        Quantity : "4 L",
+        price : "Rs 830",
+        strikedOdprice : "Rs 960"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/40195406_4-ariel-matic-front-load-liquid-detergent.jpg",
+        brand:"Surf Excel",
+        name : "Matic Front Load Liquid Detergent",
+        Quantity : "1 L",
+        price : "Rs 176.00",
+        strikedOdprice : "Rs 220.00"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/212128_26-surf-excel-matic-top-load-detergent-powder.jpg",
+        brand:"Surf Excel",
+        name : "Matic Top Load Detergent Powder",
+        Quantity : "1 kg",
+        price : "Rs 350",
+        strikedOdprice : "Rs 410"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/40130609_3-surf-excel-detergent-liquid-matic-top-load.jpg",
+        brand:"Surf Excel",
+        name : "Detergent - Liquid, Matic, Top Load",
+        Quantity : "2 L",
+        price : "Rs 359",
+        strikedOdprice : "Rs 420"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/1208976_5-bb-combo-lizol-floor-cleaner-floral-2l-harpic-toilet-cleaner-liquid-original-1l.jpg",
+        brand:"Ariel",
+        name : "Harpic Toilet Cleaner Liquid, Original 1 L + Lizol Floor & Surface Cleaner Liquid, Floral 2 L",
+        Quantity : "1 items",
+        price : "Rs 565.15",
+        strikedOdprice : "Rs 594.00"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/100134172_6-pril-dishwash-liquid-lime.jpg",
+        brand:"Ariel",
+        name : "Dishwash Liquid - Lime",
+        Quantity : "750 ml",
+        price : "Rs 159",
+        strikedOdprice : "Rs 204"
+
+    }
+]
+
+document.querySelector("#title").innerText+=" ("+dairyArr.length+")";
+
+
+displayProducts(dairyArr);
+
+function displayProducts(data){
+    data.map(function(elem,index){
+        var div1 = document.createElement("div");
+        var div2 = document.createElement("div");
+        div2.innerText="GET 20% OFF";
+        
+        var div3 = document.createElement("div");
+        div3.setAttribute("class","image-div");
+        var image = document.createElement("img");
+        image.setAttribute("src",elem.image_adress);
+        div3.append(image);
+        var div4 = document.createElement("div");
+        var brand = document.createElement("h6");
+        brand.innerText=elem.brand;
+        var a1 = document.createElement("a");
+        a1.innerText=elem.name;
+        div4.append(brand,a1);
+        var div5 = document.createElement("div");
+        var size = document.createElement("h5");
+        size.innerText=elem.Quantity;
+        var span2 = document.createElement("span");
+        var price = document.createElement("span");
+        price.innerText="Rs "+elem.price; 
+        price.style.fontWeight="bold"
+        var strikedPrice = document.createElement("span");
+        strikedPrice.innerText="Rs "+elem.strikedOdprice;
+        strikedPrice.style.textDecoration="line-through"
+        strikedPrice.style.fontSize="11px"
+        span2.append(strikedPrice,price);
+        var deleveryType = document.createElement("p");
+        deleveryType.innerHTML='<i class="fa-solid fa-truck"></i> Standard Delevery Tomorrow Morning';
+        var span4 = document.createElement("span");
+        var span5= document.createElement("span");
+        span5.innerText="Qty : ";
+        var quantity = document.createElement("input");
+        quantity.setAttribute("type","number");
+        quantity.style.width="60px"
+        var button = document.createElement("button");
+       
+       
+        button.innerHTML='<i class="fa-solid fa-cart-plus"></i>';
+        button.addEventListener("click",function(){
+            addToCart(index);
+        })
+        span4.append(span5,quantity,button);
+       
+        div5.style.padding="5px"
+        div4.style.padding="5px"
+        
+        div4.style.fontWeight="bold"
+        
+        div5.append(size,span2,deleveryType,span4);
+        div1.append(div2,div3,div4,div5)
+        document.querySelector(".products-cards").append(div1);
+
+    })
+}
+
+function sortPriceLowtoHigh(data){
+    Arrays.sort(data)
+
+}
+
+
+var cartArr = JSON.parse(localStorage.getItem("cart")) || [];
+function addToCart(index){
+    
+    var product = dairyArr.filter(function(elem,i){
+        return index==i;
+    })
+    cartArr.push(product[0]);
+    localStorage.setItem("cart",JSON.stringify(cartArr))
+
+}
---
+++ productData/eggs/products.js
@@ -0,0 +1,150 @@
+// eggs
+var dairyArr = [
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/40072320_2-fresho-farm-eggs-table-tray-medium-antibiotic-residue-free.jpg",
+        brand:"Fresho",
+        name : "Farm Eggs - Table Tray, Medium, Antibiotic Residue-Free",
+        Quantity : "12 pcs",
+        price : "Rs 79.40",
+        strikedOdprice : "Rs 110.60"
+
+    },
+    {
+        
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/1203477_2-fresho-eggs-regular.jpg",
+        brand:"Fresho",
+        name : "Eggs - Regular",
+        Quantity : "2X6 pcs",
+        price : "Rs 72.90",
+        strikedOdprice : "Rs 80.00"
+
+    },
+    
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/10000909_8-fresho-chicken-curry-cut-without-skin-antibiotic-residue-free.jpg",
+        brand:"Fresho",
+        name : "Chicken - Curry Cut Without Skin, Antibiotic Residue-Free, 6 To 9 Pcs",
+        Quantity : "250 g",
+        price : "Rs 190",
+        strikedOdprice : "Rs 226"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/1213737_1-bb-combo-fresho-farm-eggs-medium-30-pcs-kara-coconut-milk-uht-classic-imported-200-ml.jpg",
+        brand:"Fresho",
+        name : "Fresho Farm Eggs Medium 30 pcs + Kara Coconut Milk UHT Classic, Imported 200 ml",
+        Quantity : "2 item",
+        price : "Rs 254",
+        strikedOdprice : "Rs 285"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/10000900_10-fresho-chicken-breast-boneless-antibiotic-residue-free-4-5-pcs.jpg",
+        brand:"bb Combo",
+        name : "Chicken Breast - Boneless, Antibiotic Residue-Free, 2- 4 pcs",
+        Quantity : "500 g",
+        price : "Rs 259",
+        strikedOdprice : "Rs 360"
+
+    },
+    {
+        
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/1221156_1-fresho-chicken-curry-cut-without-skin-500-g-chicken-breast-boneless-500-g.jpg",
+        brand:"Fresho",
+        name : "Chicken Curry Cut Without Skin 500 g + Chicken Breast - Boneless 500 g",
+        Quantity : " 2 item",
+        price : "Rs 448",
+        strikedOdprice : "Rs 500"
+
+    },
+    {
+        image_adress:"https://www.bigbasket.com/media/uploads/p/s/1220958_1-fresho-chicken-1-kg-bb-royal-biryani-basmati-rice-extra-long-1-kg-pouch.jpg",
+        brand:"Fresho", 
+        name : "Chicken 1 kg + BB Royal Biryani Basmati Rice - Extra Long 1 kg Pouch",
+        Quantity : "2 items",
+        price : "Rs 435",
+        strikedOdprice : "Rs 605"
+
+    },
+]
+
+
+document.querySelector("#title").innerText+=" ("+dairyArr.length+")";
+
+
+displayProducts(dairyArr);
+
+function displayProducts(data){
+    data.map(function(elem,index){
+        var div1 = document.createElement("div");
+        var div2 = document.createElement("div");
+        div2.innerText="GET 20% OFF";
+        
+        var div3 = document.createElement("div");
+        div3.setAttribute("class","image-div");
+        var image = document.createElement("img");
+        image.setAttribute("src",elem.image_adress);
+        div3.append(image);
+        var div4 = document.createElement("div");
+        var brand = document.createElement("h6");
+        brand.innerText=elem.brand;
+        var a1 = document.createElement("a");
+        a1.innerText=elem.name;
+        div4.append(brand,a1);
+        var div5 = document.createElement("div");
+        var size = document.createElement("h5");
+        size.innerText=elem.Quantity;
+        var span2 = document.createElement("span");
+        var price = document.createElement("span");
+        price.innerText="Rs "+elem.price; 
+        price.style.fontWeight="bold"
+        var strikedPrice = document.createElement("span");
+        strikedPrice.innerText="Rs "+elem.strikedOdprice;
+        strikedPrice.style.textDecoration="line-through"
+        strikedPrice.style.fontSize="11px"
+        span2.append(strikedPrice,price);
+        var deleveryType = document.createElement("p");
+        deleveryType.innerHTML='<i class="fa-solid fa-truck"></i> Standard Delevery Tomorrow Morning';
+        var span4 = document.createElement("span");
+        var span5= document.createElement("span");
+        span5.innerText="Qty : ";
+        var quantity = document.createElement("input");
+        quantity.setAttribute("type","number");
+        quantity.style.width="60px"
+        var button = document.createElement("button");
+       
+       
+        button.innerHTML='<i class="fa-solid fa-cart-plus"></i>';
+        button.addEventListener("click",function(){
+            addToCart(index);
+        })
+        span4.append(span5,quantity,button);
+       
+        div5.style.padding="5px"
+        div4.style.padding="5px"
+        
+        div4.style.fontWeight="bold"
+        
+        div5.append(size,span2,deleveryType,span4);
+        div1.append(div2,div3,div4,div5)
+        document.querySelector(".products-cards").append(div1);
+
+    })
+}
+
+function sortPriceLowtoHigh(data){
+    Arrays.sort(data)
+
+}
+
+
+var cartArr = JSON.parse(localStorage.getItem("cart")) || [];
+function addToCart(index){
+    
+    var product = dairyArr.filter(function(elem,i){
+        return index==i;
+    })
+    cartArr.push(product[0]);
+    localStorage.setItem("cart",JSON.stringify(cartArr))
+
+}
